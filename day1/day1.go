30805e0e33e5995b8c5eddad230d9db47c370ae1
Salve Spinnangr <Salve.Spinnangr@skatteetaten.no>
2025-12-02 10:15:26 +0100

    Pent? Nei...

--- day1/day1.go
+++ day1/day1.go
@@ -2,6 +2,7 @@ package day1
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/Salve/AdventOfCode2025/inputs"
 	"github.com/Salve/AdventOfCode2025/registry"
@@ -10,6 +11,18 @@ import (
 const day = 1
 
 var input []byte
+
+var example = []byte(`L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+`)
 
 func init() {
 	registry.Register(day, Run)
@@ -22,11 +35,62 @@ func Run() {
 }
 
 func part1() {
-	result := "TODO"
+	d := dial{pos: 50}
+	for _, l := range inputs.Lines(input) {
+		d.turn(l)
+	}
+	result := d.landedZero
 	fmt.Printf("Part 1: %v\n", result)
 }
 
 func part2() {
-	result := "TODO"
+	d := dial{pos: 50}
+	for _, l := range inputs.Lines(input) {
+		d.turn(l)
+	}
+	result := d.landedZero + d.passedZero
 	fmt.Printf("Part 2: %v\n", result)
+}
+
+type dial struct {
+	pos        int
+	landedZero int
+	passedZero int
+}
+
+func (d *dial) turn(instruction string) {
+	v, _ := strconv.Atoi(instruction[1:])
+	d.passedZero += v / 100
+	v = v % 100
+	passed := false
+	wasZero := false
+	if d.pos == 0 {
+		wasZero = true
+	}
+	switch instruction[:1] {
+	case "L":
+		d.pos -= v
+		if d.pos < 0 {
+			d.pos += 100
+			if !wasZero {
+				passed = true
+			}
+		}
+	case "R":
+		d.pos += v
+		if d.pos > 99 {
+			d.pos -= 100
+			if !wasZero {
+				passed = true
+			}
+		}
+	}
+	if d.pos == 0 {
+		d.landedZero++
+	} else {
+		if passed {
+			d.passedZero++
+		}
+	}
+	//fmt.Printf("%s: %d (%d, %d)\n", instruction, d.pos, d.landedZero, d.passedZero)
 }
